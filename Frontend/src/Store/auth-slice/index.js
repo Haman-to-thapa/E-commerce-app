698b55e5e67ce340b7d7316286e6b78f8bb1c885
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-02-24 20:31:42 +0530

    new authUser method in and add user login also add

--- Frontend/src/Store/auth-slice/index.js
+++ Frontend/src/Store/auth-slice/index.js
@@ -30,6 +30,16 @@ export const loginUser = createAsyncThunk('/auth/login',
   }
 )
 
+// checking auth get method
+
+export const checkAuth = createAsyncThunk('/auth/checkauth',
+  async() => {
+    const response = await axios.get('http://localhost:4000/api/auth/check-auth', { withCredentials:true  , headers: {'Cache-Control' : 'no-store, no-cache, must-revalidate, proxy-revalidate', }}
+     
+    );
+    return response.data;
+  }
+)
 
 
 const authSlice = createSlice({
@@ -60,6 +70,16 @@ const authSlice = createSlice({
       state.user = action.payload.success ? action.payload.user : null;
       state.isAuthenticated = action.payload.success ? true : false;
     }).addCase(loginUser.rejected, (state,action) => {
+      state.isLoading = false;
+      state.user = null;
+      state.isAuthenticated = false;
+    }).addCase(checkAuth.pending, (state) => {
+      state.isLoading = true;
+    }).addCase(checkAuth.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.user = action.payload.success ? action.payload.user : null;
+      state.isAuthenticated = action.payload.success;
+    }).addCase(checkAuth.rejected, (state, action) => {
       state.isLoading = false;
       state.user = null;
       state.isAuthenticated = false;
