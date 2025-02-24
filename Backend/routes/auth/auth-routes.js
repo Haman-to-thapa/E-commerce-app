d9e402d8db2319b8b6d38af11402ae06c4296409
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-02-24 20:28:18 +0530

    new routes to get method to add middleware

--- Backend/routes/auth/auth-routes.js
+++ Backend/routes/auth/auth-routes.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import { authLogin, registerUser } from '../../controllers/auth/auth-controllers.js'
+import { authLogin, registerUser, logoutUser , authMiddleware} from '../../controllers/auth/auth-controllers.js'
+
 
 const authRouter = express.Router()
 
@@ -7,6 +8,16 @@ const authRouter = express.Router()
 
 authRouter.post('/register', registerUser)
 authRouter.post('/login', authLogin)
+authRouter.post('/logout',logoutUser)
+
+authRouter.get('/check-auth', authMiddleware, (req, res) => {
+  const user = req.user;
+  res.status(200).json({ 
+       success:true,
+       message:"Authenticated User!",
+       user,
+      })
+})
 
 
 
